a44f5b264da5841a19c0cd7b8e56332cc2cbe487
Ziggy <ziggyswg@outlook.com>
2018-11-04 22:47:08 +0100

    Added Urnsor'is SoulLeecher

--- src/main/java/com/projectswg/holocore/services/support/objects/items/StaticItemService.java
+++ src/main/java/com/projectswg/holocore/services/support/objects/items/StaticItemService.java
@@ -570,7 +570,7 @@ public class StaticItemService extends Service {
 
 			damageType = jsonObject.getString("damageType");
 			damageTypeEnum = getDamageTypeForName(damageType);
-			damageTypeString = "@obj_attr_n:" + damageType;
+			damageTypeString = "@obj_attr_n:armor_eff" + damageType;
 			attackSpeed = jsonObject.getFloat("attackSpeed");
 
 			maxRange = jsonObject.getInt("range");
